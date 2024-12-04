41c56beabc83bdeb7675316d37a2b28c374dd34c
yabindra-bhujel <yabindra2057@gmail.com>
2024-12-04 16:27:03 +0900

    chnage react and use api

--- resources/views/settings/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('navigation.layout')
-
-@section('title', 'Settings')
-
-@section('content')
-<h1 class="text-3xl font-semibold">Welcome to the Settings Page</h1>
-<p>Here you can manage your tasks and teams.</p>
-@endsection
--- resources/views/team/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('navigation.layout')
-
-@section('title', 'Temas')
-
-@section('content')
-<h1 class="text-3xl font-semibold">Welcome to the Team Page</h1>
-<p>Here you can manage your tasks and teams.</p>
-@endsection
