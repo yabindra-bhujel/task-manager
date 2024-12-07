ab0901fe05487ab6d59c83797fa9cbbd60bb9d10
yabindra-bhujel <yabindra2057@gmail.com>
2024-12-07 21:34:12 +0900

    add project controller

--- services/api/routes/api.php
+++ services/api/routes/api.php
@@ -1,12 +1,11 @@
-<!-- api.php -->
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 
-// auth routes
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->name('login');
     Route::post('register', 'register')->name('register');
@@ -15,10 +14,9 @@ Route::controller(AuthController::class)->group(function () {
     Route::get('me', 'me')->name('me');
 });
 
-// team routes
-Route::apiResource('teams', TeamController::class);
-Route::get('teams/{team}/members', 'TeamController@members')->name('teams.members');
-Route::post('teams/{team}/members', 'TeamController@addMember')->name('teams.addMember');
-Route::delete('teams/{team}/members/{user}', 'TeamController@removeMember')->name('teams.removeMember');
-Route::get('teams/{team}/projects', 'TeamController@projects')->name('teams.projects');
+Route::prefix('projects')->controller(ProjectController::class)->group(function () {
+    Route::post('create', 'create')->name('create');
+    Route::get('list', 'list')->name('list');
+    Route::get('show/{id}', 'show')->name('show');
+});
 
