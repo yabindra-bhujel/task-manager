82c03f0764c84d20dea7f80eb25c6b23e8a9014a
yabindra-bhujel <yabindra2057@gmail.com>
2024-12-11 12:55:27 +0900

    Task UI の実装と返信の実装

--- services/client/src/components/task/TaskCard.tsx
+++ services/client/src/components/task/TaskCard.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { FaCalendarAlt } from "react-icons/fa";
 import { BsFillExclamationCircleFill } from "react-icons/bs";
 import { Tasks } from "../type/projects/interfaces";
+import { FcNext } from "react-icons/fc";
 
-export const TaskCard = ({ task }: { task: Tasks }) => {
+export const TaskCard = ({
+  task,
+  updateTaskStatu,
+}: {
+  task: Tasks;
+  updateTaskStatu: (id: number) => void;
+}) => {
   return (
     <li className="bg-white border rounded-lg p-5 shadow-lg hover:shadow-2xl transition-all duration-300 transform">
       <div className="flex flex-col">
@@ -30,11 +37,36 @@ export const TaskCard = ({ task }: { task: Tasks }) => {
             {task.priority === "high" && (
               <BsFillExclamationCircleFill className="mr-1 inline-block" />
             )}
-            {task.priority === "medium" && "Medium Priority"}
-            {task.priority === "low" && "Low Priority"}
+            {task?.priority === "medium" && "Medium Priority"}
+            {task?.priority === "low" && "Low Priority"}
           </span>
+        </div>
+
+        <div className="flex justify-end items-center">
+          {task.status === "pending" || task.status === "in_progress" ? (
+            <button
+            onClick={() => updateTaskStatu(task.id)}
+              className={`px-2 py-1 rounded-xl ${
+                task.status === "pending"
+                  ? "bg-yellow-500 text-white hover:bg-yellow-600"
+                  : task.status === "in_progress"
+                  ? "bg-blue-500 text-white hover:bg-blue-600"
+                  : ""
+              }`}
+            >
+              <FcNext className="inline-block" size={25} />
+            </button>
+          ) : null}
         </div>
       </div>
     </li>
+  );
+};
+
+export const NoTask = ({ message }: { message: string }) => {
+  return (
+    <div className="bg-white border rounded-lg p-5 shadow-lg hover:shadow-2xl transition-all duration-300 transform">
+      <span className="text-gray-500">{message}</span>
+    </div>
   );
 };
